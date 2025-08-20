3b487d7e24f33343686ccf891faaf1fc339977a7
Hendrik Boll <fanyx@posteo.net>
2025-08-20 17:09:19 +0200

    refactor: create check_version job

---
+++ .github/scripts/check_version.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+VERSION_PREFIX="${VERSION_PREFIX:-v}"
+
+CURRENT_VERSION="$(git tag -l | tail -n 1)"
+if [[ -z "$CURRENT_VERSION" ]]; then
+  echo "No existing version tags found. Please create an initial tag manually."
+  exit 1
+fi
+
+NEW_VERSION=""
+
+IFS='.' read -ra VERSION_PARTS <<< "${CURRENT_VERSION#$VERSION_PREFIX}"
+MAJOR_VERSION=${VERSION_PARTS[0]}
+MINOR_VERSION=${VERSION_PARTS[1]}
+PATCH_VERSION=${VERSION_PARTS[2]}
+
+bump_major() {
+  MINOR_VERSION="0"
+  PATCH_VERSION="0"
+
+  MAJOR_VERSION="$((MAJOR_VERSION + 1))"
+}
+
+bump_minor() {
+  PATCH_VERSION="0"
+
+  MINOR_VERSION="$((MINOR_VERSION + 1))"
+}
+
+bump_patch() {
+  PATCH_VERSION="$((PATCH_VERSION + 1))"
+}
+
+echo "PR Labels: ${PR_LABELS}"
+
+if $BUMP_MAJOR; then
+  bump_major
+elif $BUMP_MINOR; then
+  bump_minor
+else
+  bump_patch
+fi
+
+NEW_VERSION="${VERSION_PREFIX}${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}"
+echo "New version tag: ${NEW_VERSION}"
+echo "new_version=${NEW_VERSION}" >> "${GITHUB_OUTPUT}"
